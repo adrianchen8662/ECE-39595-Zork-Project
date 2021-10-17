0f6696d91321b92a189e7a85c93cc3589190fe48
Ainesh Sootha <aineshsootha@gmail.com>
2021-10-16 22:01:45 -0400

    added funs to h

--- XMLParser.h
+++ XMLParser.h
@@ -9,3 +9,16 @@
 #include "turnon.h"
 #include "trigger.h"
 #include "border.h"
+
+Room* loadRoom(TiXmlElement* element);
+Border* loadBorder(TiXmlElement* Element);
+Condition* loadCondition(TiXmlElement* element);
+Trigger* loadTrigger(TiXmlElement* element);
+Turnon* itemTurnOn(TiXmlElement* childElement);
+Item* loadItems(TiXmlElement* element);
+Turnon* loadItemTurnOn(TiXmlElement* childElement);
+Container* loadContainers(TiXmlElement* element);
+Attack* loadAttack(TiXmlElement* element);
+Creature* loadCreatures(TiXmlElement* element);
+vector<Room*> loadXMLFile (const std::string filename);
+
